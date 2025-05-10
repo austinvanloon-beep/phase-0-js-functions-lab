4e4862fa06ce8e76291d1749b7c8d8c0bb9434d0
Austin Vanloon <wic404learn@gmail.com>
2025-05-10 10:23:50 -0600

    Initial commit

--- index.js
+++ index.js
@@ -5,3 +5,25 @@
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+function calculateTax(amount) {
+  return amount * 0.10; // 10% tax
+}
+
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
+
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
+
+function isPalindrome(word) {
+  const normalizedWord = word.toLowerCase();
+  const reversedWord = normalizedWord.split('').reverse().join('');
+  return normalizedWord === reversedWord;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  return originalPrice - (originalPrice * (discountPercentage / 100));
+}
